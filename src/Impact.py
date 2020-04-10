e81d174ab9e5616563fdc2826396489fe10a52c3
coker-felix <38159670+coker-felix@users.noreply.github.com>
2020-04-10 14:55:30 +0000

    Initial Commit

---
+++ src/Impact.py
@@ -0,0 +1,42 @@
+from InputEntry import InputData
+
+def computeForImpact(data):
+    # Create new instance of InputData class
+    impactdata = InputData(data)
+
+    # Make computations
+    # CHALLENGE 1
+    currentlyInfected = impactdata.reported_cases * 10
+    factor = impactdata.getFactor()
+    infectionsByRequestedTime = currentlyInfected * (2 ** factor)
+    infectionsByRequestedTime = round(infectionsByRequestedTime)
+
+    # CHALLENGE 2
+    severeCasesByRequestedTime = (15/100) *  infectionsByRequestedTime
+    severeCasesByRequestedTime = round(severeCasesByRequestedTime)
+    hospitalBedsByRequestedTime  = ((35/100) * impactdata.total_hospital_beds) - severeCasesByRequestedTime
+    hospitalBedsByRequestedTime = round(hospitalBedsByRequestedTime)
+
+    # CHALLENGE 2
+    casesForICUByRequestedTime = (5/100) * infectionsByRequestedTime
+    casesForICUByRequestedTime = round(casesForICUByRequestedTime)
+    casesForVentilatorsByRequestedTime = (2/100) * infectionsByRequestedTime
+    casesForVentilatorsByRequestedTime = round(casesForVentilatorsByRequestedTime)
+
+    period = impactdata.getPeriod()
+    dollarsInFlight = infectionsByRequestedTime * impactdata.average_Daily_Income_population * impactdata.average_Daily_Income_InUSD * period
+    dollarsInFlight = round(dollarsInFlight, 2)
+
+    data = {}
+    data['currentlyInfected'] = currentlyInfected
+    data['infectionsByRequestedTime'] = infectionsByRequestedTime
+    data['severeCasesByRequestedTime'] = severeCasesByRequestedTime
+    data['hospitalBedsByRequestedTime'] = hospitalBedsByRequestedTime
+    data['casesForICUByRequestedTime'] = casesForICUByRequestedTime
+    data['casesForVentilatorsByRequestedTime'] = casesForVentilatorsByRequestedTime
+    data['dollarsInFlight'] = dollarsInFlight
+    impactResult = data
+    return impactResult
+
+
+
